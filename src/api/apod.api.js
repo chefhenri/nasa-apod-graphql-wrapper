d4effbe9998ca3e54cf65083d6d3f6c617e40d26
Henry Larson <hxl1116@rit.edu>
2021-04-06 23:00:54 -0400

    Initial commit.

---
+++ src/api/apod.api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import {readFileSync} from 'fs';
+
+const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+
+// const fetchApodByDate = (apiKey, date) => {
+//     return fetch(`${BASE_URL}?api_key=${apiKey}&date=${date}`).then(res => res.json())
+// }
+
+export const typeDefs = readFileSync(`${__dirname}/apod.api.graphql`, 'utf-8');
+
+export const resolvers = {
+    Query: {
+        apod: (parent, args) => {
+            return axios.get(`${BASE_URL}?api_key=${args.apiKey}&date=${args.date}`).then(res => {
+                console.log(res.data)
+                return res.data
+            })
+        }
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {ApolloServer, makeExecutableSchema} from 'apollo-server-express';
+import {typeDefs, resolvers} from "./api/apod.api";
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+// Build Apollo server
+const server = new ApolloServer({schema});
+server.applyMiddleware({app});
+
+// Run server
+app.listen({port}, () => {
+    console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`)
+})
