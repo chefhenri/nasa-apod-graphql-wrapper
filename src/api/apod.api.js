47ff0469c5a49a26131f06431afc53da81c9df87
Henry Larson <hxl1116@rit.edu>
2021-04-10 15:45:08 -0400

    Added resolvers for 'today', 'apodByDate', 'apodsByDate', and 'randomApods'

--- src/api/apod.api.js
+++ src/api/apod.api.js
@@ -6,28 +6,50 @@ const typeDefs = fs.readFileSync(`${__dirname}/apod.api.gql`, 'utf-8');
 const resolvers = {
     Query: {
         today: (parent, args) => {
-            // TODO: Build request url conditionally
-            return axios.get(`${process.env.BASE_URL}?api_key=${args.apiKey}`)
+            return axios.get(buildUrl(args))
                 .then(res => {
                     console.log(res.data);
                     return res.data;
                 })
         },
         apodByDate: (parent, args) => {
-            // TODO: Build request url conditionally
-            return axios.get(`${process.env.BASE_URL}?api_key=${args.apiKey}&date=${args.date}`)
+            return axios.get(buildUrl(args))
                 .then(res => {
-                    console.log(res.data)
-                    return res.data
+                    console.log(res.data);
+                    return res.data;
                 })
         },
         apodsByDate: (parent, args) => {
-            // TODO: Build request url
+            return axios.get(buildUrl(args))
+                .then(res => {
+                    console.log(res.data);
+                    return res.data;
+                })
         },
         randomApods: (parent, args) => {
-            // TODO: Build request url
+            return axios.get(buildUrl(args))
+                .then(res => {
+                    console.log(res.data);
+                    return res.data;
+                })
         }
     }
+}
+
+// Converts camel case JSON key to snake case HTTP param format
+const convertArgCase = arg => {
+    return arg.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+}
+
+// Builds URL params with query args
+const buildUrl = args => {
+    let params = []
+    for (let arg in args) {
+        if (args.hasOwnProperty(arg))
+            params.push(`${convertArgCase(arg)}=${args[arg]}`)
+    }
+
+    return `${process.env.BASE_URL}?${params.join('&')}`
 }
 
 module.exports = {
