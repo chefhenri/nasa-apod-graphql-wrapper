6d1a1b67e9fb28b83b6fd7e95d4752e811b059da
Henry Larson <hxl1116@rit.edu>
2021-04-10 12:27:16 -0400

    Updated schema, stubbed out resolvers.

--- src/api/apod.api.js
+++ src/api/apod.api.js
@@ -5,11 +5,27 @@ const typeDefs = fs.readFileSync(`${__dirname}/apod.api.gql`, 'utf-8');
 
 const resolvers = {
     Query: {
-        apod: (parent, args) => {
-            return axios.get(`${process.env.BASE_URL}?api_key=${args.apiKey}&date=${args.date}`).then(res => {
-                console.log(res.data)
-                return res.data
-            })
+        today: (parent, args) => {
+            // TODO: Build request url conditionally
+            return axios.get(`${process.env.BASE_URL}?api_key=${args.apiKey}`)
+                .then(res => {
+                    console.log(res.data);
+                    return res.data;
+                })
+        },
+        apodByDate: (parent, args) => {
+            // TODO: Build request url conditionally
+            return axios.get(`${process.env.BASE_URL}?api_key=${args.apiKey}&date=${args.date}`)
+                .then(res => {
+                    console.log(res.data)
+                    return res.data
+                })
+        },
+        apodsByDate: (parent, args) => {
+            // TODO: Build request url
+        },
+        randomApods: (parent, args) => {
+            // TODO: Build request url
         }
     }
 }
